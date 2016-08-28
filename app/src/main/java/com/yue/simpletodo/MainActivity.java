f9cafa231444c62aee04cf7399dbad9e2691cbab
yue8757@gmail.com <yue8757@gmail.com>
2016-08-28 14:39:21 -0400

    3. Mock data.

--- app/src/main/java/com/yue/simpletodo/MainActivity.java
+++ app/src/main/java/com/yue/simpletodo/MainActivity.java
@@ -1,7 +1,14 @@
 package com.yue.simpletodo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+
+import com.yue.simpletodo.models.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @NonNull
+    private List<Todo> mockData() {
+        List<Todo> list = new ArrayList<>();
+        for (int i = 0; i < 1000; ++i) {
+            list.add(new Todo("todo " + i));
+        }
+        return list;
     }
 }
---
+++ app/src/main/java/com/yue/simpletodo/models/Todo.java
@@ -0,0 +1,12 @@
+package com.yue.simpletodo.models;
+
+public class Todo {
+
+    public boolean done;
+
+    public String text;
+
+    public Todo(String text) {
+        this.text = text;
+    }
+}
